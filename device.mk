eec761f1ba2ffe9cab56a975779fae254291d6f9
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-19 00:17:36 +0000

    sweet: props: Configure dynamic refresh rate props
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -229,6 +229,36 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
 
+# Refresh Rate Dynamic Props
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms?=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=0 \
+    debug.sf.enable_gl_backpressure=1 \
+    vendor.display.idle_time=0 \
+    vendor.display.idle_time_inactive=0
+
+# Enable app/sf phase offset as durations. The numbers below are translated from the existing
+# positive offsets by finding the duration app/sf will have with the offsets.
+# For SF the previous value was 6ms which under 16.6ms vsync time (60Hz) will leave SF with ~10.5ms
+# for each frame. For App the previous value was 2ms which under 16.6ms vsync time will leave the
+# App with ~20.5ms (16.6ms * 2 - 10.5ms - 2ms). The other values were calculated similarly.
+# Full comparison between the old vs. the new values are captured in
+# https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.frame_rate_multiple_threshold=120 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=16000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000
+
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
